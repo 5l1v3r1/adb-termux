600eb2115c5bfa0f0ca62f24f5f9acb7076235da
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-05-24 22:08:27 +0530

    Update adb-server.sh

--- adb-server.sh
+++ adb-server.sh
@@ -5,8 +5,6 @@ echo
 echo -e "\e[93mThis script will install ADB & FastBoot Tools in Termux."
 echo
 echo -e "\e[32m[*] \e[34mDownloading wrapper script..."
-mkdir $PREFIX/tmp/adbtemp
-downpath="$PREFIX/tmp/adbtemp"
 wget https://github.com/ShuBhamg0sain/adb-server/raw/master/bin/adb -P $downpath/ -q
 echo -e "\e[32m[*] \e[34mDownloading binaries..."
 wget https://github.com/ShuBhamg0sain/adb-server/raw/master/bin/adb.bin -P $downpath/ -q
@@ -15,19 +13,17 @@ wget https://github.com/ShuBhamg0sain/adb-server/raw/master/bin/fastboot-armeabi
 echo -e "\e[32m[*] \e[34mCopying files to bin..."
 cd bin
 echo -e "\e[32m[*] \e[34mSetting execution permissions..."
-chmod +x 
-chmod +x 
-chmod +x 
-chmod +x 
-chmod +x 
+chmod +x adb
+chmod +x adb.bin
+chmod +x fastboot
+chmod +x fastboot-armeabi
 echo -e "\e[32m[*] \e[34mCreating workspace directory..."
 cd $HOME
 if [ ! -d "adbfiles" ]; then
   mkdir adbfiles
 fi
 echo -e "\e[32m[*] \e[34mCleaning up..."
-cd $directory
-rm -rf $downpath
+cd adb-server
 rm -rf adb-server.sh
 echo
 echo -e "\e[32mTools were successfully installed!\e[39m"
