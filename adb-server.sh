5996ae5599669f041dbcfa313e8cec8b99161d9c
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-05-25 08:08:29 +0530

    Update adb-server.sh

--- adb-server.sh
+++ adb-server.sh
@@ -7,11 +7,11 @@ echo
 echo -e "\e[32m[*] \e[34mDownloading wrapper script..."
 mkdir $PREFIX/tmp/adbtemp
 downpath="$PREFIX/tmp/adbtemp"
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/adb -P $downpath/ -q
+wget https://raw.githubusercontent.com/ShuBhamg0sain/adb-server/main/bin/adb -P $downpath/ -q
 echo -e "\e[32m[*] \e[34mDownloading binaries..."
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/adb.bin -P $downpath/ -q
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/fastboot -P $downpath/ -q
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/fastboot-armeabi -P $downpath/ -q
+wget https://raw.githubusercontent.com/ShuBhamg0sain/adb-termux/main/bin/adb.bin -P $downpath/ -q
+wget https://raw.githubusercontent.com/ShuBhamg0sain/adb-termux/main/bin/fastboot -P $downpath/ -q
+wget https://raw.githubusercontent.com/ShuBhamg0sain/adb-termux/main/bin/fastboot-armeabi -P $downpath/ -q
 echo -e "\e[32m[*] \e[34mCopying files to bin..."
 cp $downpath/* $PREFIX/bin
 echo -e "\e[32m[*] \e[34mSetting execution permissions..."
@@ -26,7 +26,7 @@ fi
 echo -e "\e[32m[*] \e[34mCleaning up..."
 cd $directory
 rm -rf $downpath
-rm -rf InstallTools.sh
+rm -rf install.sh
 echo
 echo -e "\e[32mTools were successfully installed!\e[39m"
 echo
